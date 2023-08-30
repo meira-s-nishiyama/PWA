37a9494d5f52f8e35e6974b5a0dbf69d81faa852
meira-s-nishiyama <142953272+meira-s-nishiyama@users.noreply.github.com>
2023-08-30 16:26:22 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+// Change this to your repository name
+const GHPATH = '/PWA';
 
+// Choose a different app prefix name
+const APP_PREFIX = 'pwa_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+const VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+const URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/styles.css`,
+  `${GHPATH}/script.js`
+];
